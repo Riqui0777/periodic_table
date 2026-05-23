75e5a7ca069b45346de454c18c1809ce75a14677
Riqui0777 <peperodlo25@gmail.com>
2026-05-23 19:18:56 -0400

    refactor: better readability

--- element.sh
+++ element.sh
@@ -11,6 +11,7 @@ else
   else 
    ELEMENTS=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, types.type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING (type_id) WHERE symbol = '$1' OR name = '$1'") 
   fi 
+
   if [[ -z $ELEMENTS ]]
   then
     echo "I could not find that element in the database." 
@@ -19,5 +20,6 @@ else
     do
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     done
-  fi    
+  fi
+
 fi
