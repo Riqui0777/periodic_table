08f660e4e5f255ed735171649831e7702c6e3ecf
Riqui0777 <peperodlo25@gmail.com>
2026-05-23 19:15:59 -0400

    fix: delete line break

--- element.sh
+++ element.sh
@@ -17,7 +17,7 @@ else
   else
     echo $ELEMENTS | while IFS='|' read -r ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE; 
     do
-      echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius.\n"
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     done
   fi    
 fi
